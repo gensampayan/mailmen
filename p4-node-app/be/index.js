a5af93259011b4c5e9fe72840ea25d31ae6cb766
geneno00 <geneno.sampayan@gmail.com>
2024-07-16 23:24:19 +0800

    updated: allow origins 2

--- p4-node-app/be/index.js
+++ p4-node-app/be/index.js
@@ -15,24 +15,9 @@ const baseUrl = "/api/v1";
 
 db();
 app.use(helmet());
-
-const allowedOrigins = [
-  'https://mailmen.vercel.app',
-  'https://mailmen-zvco1rqi0-gens-projects-9f40b61e.vercel.app'
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type, Authorization"
+  origin: ['https://mailmen.vercel.app', 'https://mailmen-zvco1rqi0-gens-projects-9f40b61e.vercel.app']
 }));
-
 app.use(express.json());
 app.use(`${baseUrl}/users`, userRouter);
 app.use(`${baseUrl}/emails`, emailRouter);
