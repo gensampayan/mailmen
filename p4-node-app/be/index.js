9638debc1779ea14e30c33e2e7a682224de1b15b
geneno00 <geneno.sampayan@gmail.com>
2024-07-16 22:38:34 +0800

    update: be api2

--- p4-node-app/be/index.js
+++ p4-node-app/be/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import helmet from "helmet"
+import helmet from "helmet";
 import db from "./config/db.js";
 import userRouter from "./routes/user.route.js";
 import emailRouter from "./routes/email.route.js";
@@ -11,16 +11,33 @@ import { pageNotFound, errorHandler } from "./middlewares/error.middleware.js";
 dotenv.config();
 
 const app = express();
-const baseUrl = "https://mailmen-nz6kqcpk6-gens-projects-9f40b61e.vercel.app/api/v1";
+const baseUrl = "/api/v1";
 
 db();
 
-app.use(cors());
+// CORS configuration
+const allowedOrigins = [
+  'https://mailmen-2ufjnw4lc-gens-projects-9f40b61e.vercel.app', 
+  'https://your-other-allowed-origin.com'
+];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 app.use(express.json());
 app.use(helmet());
+
 app.use(`${baseUrl}/users`, userRouter);
 app.use(`${baseUrl}/emails`, emailRouter);
 app.use(`${baseUrl}/mailbox`, mailboxRouter);
+
 app.use(pageNotFound);
 app.use(errorHandler);
 
