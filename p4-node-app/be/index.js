cd20fff2e5d0c079403f9b9dad3855f78bae1978
geneno00 <geneno.sampayan@gmail.com>
2024-07-16 22:45:28 +0800

    update: be api3

--- p4-node-app/be/index.js
+++ p4-node-app/be/index.js
@@ -14,30 +14,14 @@ const app = express();
 const baseUrl = "/api/v1";
 
 db();
-
-// CORS configuration
-const allowedOrigins = [
-  'https://mailmen-2ufjnw4lc-gens-projects-9f40b61e.vercel.app', 
-  'https://your-other-allowed-origin.com'
-];
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
+  origin: "https://mailmen-2ufjnw4lc-gens-projects-9f40b61e.vercel.app"
 }));
-
 app.use(express.json());
 app.use(helmet());
-
 app.use(`${baseUrl}/users`, userRouter);
 app.use(`${baseUrl}/emails`, emailRouter);
 app.use(`${baseUrl}/mailbox`, mailboxRouter);
-
 app.use(pageNotFound);
 app.use(errorHandler);
 
